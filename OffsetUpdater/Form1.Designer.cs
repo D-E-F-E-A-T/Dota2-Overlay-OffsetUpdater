e59efedeb1b58e68ab4cf9a2e32adf29ba649f6c
Skrixx68 <35738176+skrixx68@users.noreply.github.com>
2020-06-17 20:00:00 +0800

    release

---
+++ OffsetUpdater/Form1.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace OffsetUpdater
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lbl_modDate = new System.Windows.Forms.Label();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lbl_stats = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(143, 88);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(80, 30);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Update";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(23, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(105, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Last Update :";
+            // 
+            // lbl_modDate
+            // 
+            this.lbl_modDate.AutoSize = true;
+            this.lbl_modDate.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_modDate.Location = new System.Drawing.Point(139, 33);
+            this.lbl_modDate.Name = "lbl_modDate";
+            this.lbl_modDate.Size = new System.Drawing.Size(18, 20);
+            this.lbl_modDate.TabIndex = 2;
+            this.lbl_modDate.Text = "0";
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Location = new System.Drawing.Point(250, 48);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(115, 97);
+            this.webBrowser1.TabIndex = 3;
+            this.webBrowser1.Visible = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(24, 132);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(43, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Status :";
+            // 
+            // lbl_stats
+            // 
+            this.lbl_stats.AutoSize = true;
+            this.lbl_stats.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_stats.Location = new System.Drawing.Point(73, 132);
+            this.lbl_stats.Name = "lbl_stats";
+            this.lbl_stats.Size = new System.Drawing.Size(62, 13);
+            this.lbl_stats.TabIndex = 5;
+            this.lbl_stats.Text = "Waiting...";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(377, 157);
+            this.Controls.Add(this.lbl_stats);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.webBrowser1);
+            this.Controls.Add(this.lbl_modDate);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Updater";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lbl_modDate;
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lbl_stats;
+    }
+}
+
---
+++ OffsetUpdater/Form1.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OffsetUpdater
+{
+    public partial class Form1 : Form
+    {
+        //Config Directory
+        public string confPath = Environment.CurrentDirectory + @"\offs.conf";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            getFileInfo();
+        }
+
+        // get config file last modified date
+        private void getFileInfo()
+        {
+            if (File.Exists(confPath))
+            {
+                DateTime modification = File.GetLastWriteTime(confPath);
+                lbl_modDate.Text = modification.Date.ToString("MM/dd/yyyy");
+            }
+            else
+            {
+                MessageBox.Show("Config file not found , Press enter to create new config");
+                File.Create(confPath);
+                Application.Restart();
+            }
+        }
+
+        // Write the offsets to our config file.
+        private void writeOffsets()
+        {
+            string[] temp = rawOffsets.Split();
+            string offset = "";
+            for (int i = 0; i < temp.Length; i++)
+            {
+                offset = offset + " " + temp[i];
+            }
+            temp = null;
+            File.WriteAllText(confPath, offset);
+            lbl_stats.Text = "Done.";
+            getFileInfo();
+        }
+
+        //Start updating offset from dataserver.
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            lbl_stats.Text = "Fetching Latest Offsets , Please Wait";
+            Thread.Sleep(100);
+            webBrowser1.ScriptErrorsSuppressed = true;
+            webBrowser1.Navigate("https://pastebin.com/Gf8T1DDn");
+            //wait for page loading
+            await PageLoad(10);
+            string urlOffs;
+            HtmlElement htmlelement = webBrowser1.Document.GetElementById("paste_code");
+            if (htmlelement != null)
+            {
+                urlOffs = webBrowser1.Document.GetElementById("paste_code").OuterText;
+                rawOffsets = urlOffs;
+                htmlelement = null;
+                webBrowser1.Dispose();
+                writeOffsets();
+            }
+            else
+            {
+                MessageBox.Show("Loading data site failed.");
+            }
+        }
+
+        private string _rawOffs;
+        public string rawOffsets
+        {
+            get { return _rawOffs; }
+            set { _rawOffs = value; } 
+        }
+
+        private async Task PageLoad(int TimeOut)
+        {
+            TaskCompletionSource<bool> PageLoaded = null;
+            PageLoaded = new TaskCompletionSource<bool>();
+            int TimeElapsed = 0;
+            webBrowser1.DocumentCompleted += (s, e) =>
+            {
+                if (webBrowser1.ReadyState != WebBrowserReadyState.Complete) return;
+                if (PageLoaded.Task.IsCompleted) return; PageLoaded.SetResult(true);
+            };
+            
+            while (PageLoaded.Task.Status != TaskStatus.RanToCompletion)
+            {
+                await Task.Delay(10);
+                TimeElapsed++;
+                if (TimeElapsed >= TimeOut * 100) PageLoaded.TrySetResult(true);
+            }
+        }
+
+    }
+}
